a040785fbb19026c5d29b3960b8ada0b961c2340
kososd <kososd@gmail.com>
2017-08-27 22:47:20 +0100

    Initial upload

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,25 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current {get;} = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto{ ID = 1, Name = "Limerick", Description = "City were I live."},
+                new CityDto{ ID = 2, Name = "Dublin", Description = "Capital of Ireland."},
+                new CityDto{ ID = 3, Name = "Cork", Description = "City were my kids go to school."},
+            };
+        }
+
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("API/Cities")]
+    public class CitiesController:Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var temp = CitiesDataStore.Current.Cities.Where(c => c.ID == id).FirstOrDefault();
+            if (temp == null)
+            {
+                return NotFound();
+            } else
+            {
+                return Ok(temp);
+            }
+
+        }
+    }
+}
